a61aad5116dd76c9ca59961927d1dd1788b17614
Michiel Van Himbeeck <michiel.vanhimbeeck@student.odisee.be>
2024-01-24 17:50:30 +0100

    readme and auth store

--- instances/myAxios.ts
+++ instances/myAxios.ts
@@ -9,4 +9,13 @@ const myAxios = axios.create({
   //proxy: false
 });
 myAxios.defaults.withCredentials = false;
+
+myAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = "Bearer " + token;
+  }
+
+  return config;
+});
 export { myAxios };
