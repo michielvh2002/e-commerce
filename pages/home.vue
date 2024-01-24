7e7a109d6d916d23e749fdaaa667d79fdddfb33f
Michiel Van Himbeeck <michiel.vanhimbeeck@student.odisee.be>
2024-01-24 19:47:43 +0100

    push it

--- pages/home.vue
+++ pages/home.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ShopSchema } from "~/types/ShopSchema";
+
+const shops = ref<Array<ShopSchema>>([]);
+(async () => {
+  const store = useShopStore();
+
+  try {
+    const res = await store.getShops();
+    console.log(res.data.data);
+    shops.value = res.data.data;
+  } catch (error) {}
+})();
+</script>
 
 <template>
-  <div>Home</div>
+  <div>
+    <h1>Home</h1>
+
+    <h2>shops</h2>
+    <ul>
+      <li v-for="(shop, index) in shops">
+        <h3>{{ shop.name }}</h3>
+        <img src="~/assets/placeholder.jpg" alt="placeholder" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- stores/shopStore.ts
+++ stores/shopStore.ts
@@ -7,19 +7,25 @@ export const useShopStore = defineStore("shop", () => {
   const createShop = async (
     shop: ShopSchema
   ): Promise<AxiosResponse<any, any>> => {
-    const res = await myAxios.post("/createShop", { shop });
+    console.log("hello");
+    const res = await myAxios.post("/shop", { shop });
     return res;
   };
 
   const createItem = async (
     item: ItemSchema
   ): Promise<AxiosResponse<any, any>> => {
-    const res = await myAxios.post("/createItem", { item });
+    const res = await myAxios.post("/item", { item });
     return res;
+  };
+
+  const getShops = async (): Promise<AxiosResponse<any, any>> => {
+    return myAxios.get("/shops");
   };
 
   return {
     createShop,
     createItem,
+    getShops,
   };
 });
